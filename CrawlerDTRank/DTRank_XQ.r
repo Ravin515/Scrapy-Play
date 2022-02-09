30d4c20b528ca0b2b980089b2ee035a653d168b5
Mr.Stylee <ray.stylee@outlook.com>
2022-02-09 15:29:30 +0800

    update 20220209

--- CrawlerDTRank/DTRank_XQ.r
+++ CrawlerDTRank/DTRank_XQ.r
@@ -1,15 +1,15 @@
 library(styleer)
-# DTRank <- fbread(pattern = "*.csv", encoding = "UTF-8")
-# DTRank <- DTRank[, file_id := NULL, 
-#     ][, unique(.SD)
-#     ][, stock_symbol := str_pad(stock_symbol, width = 6, side = "left", pad = "0")
-#     ][order(-date, stock_symbol), .SD
-#     ]
+DTRank <- fbread(pattern = "*.csv", encoding = "UTF-8")
+DTRank <- DTRank[, file_id := NULL, 
+    ][, unique(.SD)
+    ][, stock_symbol := str_pad(stock_symbol, width = 6, side = "left", pad = "0")
+    ][order(-date, stock_symbol), .SD
+    ]
+fwrite(DTRank, "DTRank.csv")
 
-# fwrite(DTRank, "DTRank.csv")
-trading <- fread("./CrawlerDTRank/TRD_Dalyr.txt", encoding = "UTF-8")
+trading <- fread("TRD_Dalyr.txt", encoding = "UTF-8")
 setnames(trading, 1:2, c("stock_symbol", "date"))
-dtrank <- fread("./CrawlerDTRank/DTRank.csv", encoding = "UTF-8")
+dtrank <- fread("DTRank.csv", encoding = "UTF-8")
 dtanly <- dtrank[trading, on = .(stock_symbol, date)
     ][order(stock_symbol, date), .SD
     ][, stock_symbol := str_pad(stock_symbol, 6, side = "left", pad = "0")
