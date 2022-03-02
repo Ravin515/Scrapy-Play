5f8ae4ae6e52e99581ace3db1cff8912c51898a9
Mr.Stylee <ray.stylee@outlook.com>
2022-03-02 15:33:29 +0800

    update

--- CrawlerDTRank/DTRank_XQ.r
+++ CrawlerDTRank/DTRank_XQ.r
@@ -10,10 +10,18 @@ fwrite(DTRank, "DTRank.csv")
 trading <- fread("TRD_Dalyr.txt", encoding = "UTF-8")
 setnames(trading, 1:2, c("stock_symbol", "date"))
 dtrank <- fread("DTRank.csv", encoding = "UTF-8")
-dtanly <- dtrank[trading, on = .(stock_symbol, date)
+buy <- dtrank[trading, on = .(stock_symbol, date)
     ][order(stock_symbol, date), .SD
     ][, stock_symbol := str_pad(stock_symbol, 6, side = "left", pad = "0")
     ][, ret := Clsprc/shift(Clsprc) - 1, by = .(stock_symbol)
     ][!is.na(ret), .SD
     ][ret > 0.09 & buy_inst_num > sell_inst_num, .SD
+    ]
+
+sell <- dtrank[trading, on = .(stock_symbol, date)
+    ][order(stock_symbol, date), .SD
+    ][, stock_symbol := str_pad(stock_symbol, 6, side = "left", pad = "0")
+    ][, ret := Clsprc/shift(Clsprc) - 1, by = .(stock_symbol)
+    ][!is.na(ret), .SD
+    ][buy_inst_num < sell_inst_num, .SD
     ]
